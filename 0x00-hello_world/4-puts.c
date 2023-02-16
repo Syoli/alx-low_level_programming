3b6c7bd9ef28899300842eb8e9992148d6c93be2
syoli <siyolipama@gmail.com>
2023-02-16 08:12:47 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,12 @@
 
 /**
  * main - Entry point
- * Return: always 0 (Success)
+ *
+ * Return: always 0 (Successful)
  */
 
 int main(void)
 {       
-        puts("Programming is like building a multilingual puzzle\n");
-        Return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}	
