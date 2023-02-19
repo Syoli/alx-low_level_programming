60de2d9b2dc074abd197bfa2ac8298691c74142a
syoli <siyolipama@gmail.com>
2023-02-19 10:39:19 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
-	
-	#include <ctype.h>
-	/**
-	 * main - main block
-	 * Description: Get a random number and print the number
-	 * positive,negative or zero
-	 * Return: Always 0 (success)
-	 */
-	int main(void)
+#include <ctype.h>
+
+/**
+ * main - Get a random number and print the number
+ * positive,negative or zero
+ * Return: Always 0 (success)
+ */
+int main(void)
 	{
-		int digit;
-	
-		for (digit = '0'; digit <= '9'; digit++)
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
 		{
-			putchar(digit);
-			if (digit == '9')
-				continue;
-	
-			putchar(',');
-			putchar(' ');
+		putchar(digit);
+		if (digit == '9')
+			continue;
+
+		putchar(',');
+		putchar(' ');
 		}
 		putchar('\n');
 		return (0);
