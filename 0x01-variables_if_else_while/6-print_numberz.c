438b9a74971758c54ff6a507671916ff1304286b
syoli <siyolipama@gmail.com>
2023-02-19 10:21:12 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-	#include <ctype.h>
-	
-	/**
-	 * main - Entry point
-	 * a program that prints all single digit numbers of base 10 starting from 0
-	 * followed by a new line
-	 * Return: Always 0 (success)
-	 */
-	int main(void)
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * a program that prints all single digit numbers of base 10 starting from 0
+ * followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
 	{
-		int number;
-	
+	int number;
+
 		for (number = '0'; number <= '9'; number++)
 		{
 			putchar(number);
