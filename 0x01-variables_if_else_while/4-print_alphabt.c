58be591c9cb558a669eb262f692e94db1e8a9cf3
syoli <siyolipama@gmail.com>
2023-02-19 10:12:04 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
-	#include <ctype.h>
-	
-	/**
-	 * main - Entry point
-	 * Write a program that prints the alphabet in lowercase
-	 * followed by a new line
-	 * Return: Always 0 (success)
-	 */
-	
-	int main(void)
+#include <ctype.h>
+
+/**
+ * main - Write a program that prints the alphabet in lowercase
+ * followed by a new line
+ * Return: Always 0 (success)
+ */
+
+int main(void)
 	{
-		int letter;
-	
-		for (letter = 'a'; letter <= 'z'; letter++)
-		{
-			if (letter == 'q' || letter == 'e')
-			{
-				continue;
-			}
-			putchar(letter);
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	if (letter == 'q' || letter == 'e')
+	{
+	continue;
+	}
+		putchar(letter);
 		}
 		putchar('\n');
 		return (0);
