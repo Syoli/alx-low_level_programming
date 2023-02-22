94f7669db01308f3d13004921e68358444e55173
Syoli <122825084+Syoli@users.noreply.github.com>
2023-02-22 15:37:22 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,7 +9,7 @@ void print_alphabet(void)
 {
         char letter;
 
-        for (c = 'a'; c <= 'z'; letter++)
+        for (letter = 'a'; letter = 'z'; letter++)
         {
                 _putchar(letter);
         }
