d58911333db841cfcfc027cfcd5971fdea68d8f6
syoli <siyolipama@gmail.com>
2023-02-22 06:52:18 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code for ALX School students.
+ * main - Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
  *
  * Return: Always 0.
  */
