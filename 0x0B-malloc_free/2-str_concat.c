a48ee906f57a4fbd4cd0f808299ae1966de4cf06
syoli <siyolipama@gmail.com>
2023-03-14 11:45:07 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,62 +1,41 @@
 #include "main.h"
-/**
- *_strlen - count arrray
- *@s: array of elements
- *Return: i
- */
-
-int _strlen(char *s)
-{
-	unsigned int i;
-
-	i = 0;
-	while (s[i] != '\0') /*Count character of string*/
-	{
-		i++;
-	}
-
-	return (i);
-}
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- *str_concat - back a pointer to array
- *@s1: Array one
- *@s2: Array two
- *Return: Always an array dinamic
+ * alloc_grid - Main Enry
+ * @width: input
+ * @height: input
+ * Return: 0
  */
 
-char *str_concat(char *s1, char *s2)
+int **alloc_grid(int width, int height)
 {
-	char *dst;
-	unsigned int i, j, size;
+	int **grid;
+	int i, j;
 
-	/*If the array is empty*/
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	/*count size total*/
-	size = (_strlen(s1) + _strlen(s2) + 1);
-
-	/*malloc*/
-	dst = (char *) malloc(size * sizeof(char));
-
-	if (dst == 0)
-	{
+	if (width <= 0 || height <= 0)
 		return (NULL);
-	}
 
-	/*Concatenate arrays*/
-	for (i = 0; *(s1 + i) != '\0'; i++)
-		*(dst + i) = *(s1 + i);
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
 
-	for (j = 0; *(s2 + j) != '\0'; j++)
+	for (i = 0; i < height; i++)
 	{
-		*(dst + i) = *(s2 + j);
-		i++;
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			while (i >= 0)
+				free(grid[i--]);
+			free(grid);
+			return (NULL);
+		}
+
+
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
 	}
 
-	return (dst);
+	return (grid);
 }
