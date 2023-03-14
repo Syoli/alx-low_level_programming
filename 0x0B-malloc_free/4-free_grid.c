36e211227ac640c278460af1bf553cf205737de5
syoli <siyolipama@gmail.com>
2023-03-14 11:37:15 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -6,14 +6,9 @@
  * Return: free grid
  */
 
-void free_grid(int **grid, int height)
-{
-	int i;
-
-	for (i = 0; i < height; i++)
-	{
-		free(grid[i]);
-	}
-
-	free(grid);
+void free_grid(int **grid, int height) {
+    for (int i = 0; i < height; i++) {
+        free(grid[i]);
+    }
+    free(grid);
 }
